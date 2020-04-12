22f2de48d3798e86f46a958ea37ab95a1812ffbb
Mick Farrow <mick.farrow@visualdev.net>
2020-04-12 11:05:30 -0500

    Tweaks

--- FileRead/FileReader.java
+++ FileRead/FileReader.java
@@ -1,7 +1,11 @@
 
-//import required java classes
+/*
+Like most programming languages there is built in functionality in java
+To use the built in functionality you import the files into your code
+This is done at the top of the page
+import required java classes
+*/
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,12 +15,21 @@ class FileReader {
     public static void main(String[] args) {
         
         //string variable containing filename
-        String inputFile = "inputFile.txt";
+        String inputFile = "inputFile1.txt";
 
         //Create ArrayList (java.util) to hold results
         ArrayList<String> lines = new ArrayList<String>();
 
-        //use try...catch block to fill out the ArrayList
+        /*
+        use try...catch block to fill out the ArrayList
+        try {
+        }
+        catch(Exception e){
+        }
+        
+        You place the code inside the try block, if an error happens an Exception is thrown and this is caught and handled by the catch block
+        The catch block receives the Exception as a parameter and you can read it, and determine what to do.
+        */
         try {
 
             //create file object using inputFile variable (this is why you import java.io.File;)
@@ -43,11 +56,12 @@ class FileReader {
             //Your program has the file open, you must release it
             fileScanner.close();
         } 
-        catch (FileNotFoundException e) {
+        catch (Exception e) {
             //force error by changing above line to String inputFile = "doesnotexist.txt";
             System.out.println("Error: " + e.getMessage());
-            //A stack trace is a detailed account of the error, very useful if the error is
-            //e.printStackTrace();
+            //A stack trace is a detailed account of the error, can be very useful in determining the cause of the error
+            e.printStackTrace();
+            //because we have handled the error, the program will continue, if we didn't have a try..catch block the program would fail
         }
 
         //write out the array, then write each line out
